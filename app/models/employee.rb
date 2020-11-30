0b88e5808a7aa1538e5deca915ac4122c244abfb
Mario <a01187827@itesm.mx>
2020-11-29 18:39:26 -0700

    added relationship between user and employee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,5 @@
 class Employee < ApplicationRecord
     has_many :teams, dependent: :destroy
     has_many :projects, through: :teams
+    belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_29_221124) do
+ActiveRecord::Schema.define(version: 2020_11_30_010816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,9 +27,10 @@ ActiveRecord::Schema.define(version: 2020_11_29_221124) do
   create_table "employees", force: :cascade do |t|
     t.string "name"
     t.string "role"
-    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_employees_on_user_id"
   end
 
   create_table "projects", force: :cascade do |t|
@@ -69,6 +70,7 @@ ActiveRecord::Schema.define(version: 2020_11_29_221124) do
   end
 
   add_foreign_key "comments", "tickets"
+  add_foreign_key "employees", "users"
   add_foreign_key "teams", "employees"
   add_foreign_key "teams", "projects"
   add_foreign_key "tickets", "projects"
